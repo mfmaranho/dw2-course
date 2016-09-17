d1126aca51b9b7e29138ac4c9891cd59a1668781
Eduardo Pedroso <drdpedroso@gmail.com>
2016-09-17 19:30:53 -0300

    Terminando revisao

--- exercicios-prova/index.php
+++ exercicios-prova/index.php
@@ -2,23 +2,22 @@
 	<header>
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
 	</header>
-	<?php $conn = mysqli_connect("localhost", "root", "root", "revisao");
-	$estados = mysqli_query($conn, "SELECT 'nome' FROM 'estado'");
-	print $estados;
-	 ?>
+
 	<body>
+		<?php $conn = mysqli_connect("localhost", "root", "root", "revisao");
+		$result = mysqli_query($conn, "SELECT * FROM estado");
+		$resul = mysqli_fetch_all($result);
+
+		 ?>
         <form method="post" action="Cadastro.php">
             <input type="text" name="nome" placeholder="Nome" />
 						<select name='estado'>
 							<?php
-								foreach ($adados as $opt)
-								{
-									$select .= "<option value={$opt[1]}> {$opt[0]} </option>";
+								foreach ($resul as $r) {
+									echo "<option>" . $r[2] . "</option>";
 								}
-								echo $select;
 							?>
 						</select>
-
 
             <input type="submit" name="doCalc" value="Salvar" />
         </form>
